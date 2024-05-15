61bea80cfd4a3b2c822391f2c66fe1fb9054246b
Sujit Sarkar <snigdho532@gmail.com>
2024-05-15 19:38:55 +0600

    Unnecessary widget removed

--- lib/shared/widgets/widgets_imports.dart
+++ lib/shared/widgets/widgets_imports.dart
@@ -6,10 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh_new/pull_to_refresh.dart';
 import '../../core/constants/app_color.dart';
-
-part 'custom_outline_button.dart';
-part 'solid_button.dart';
-part 'custom_text_button.dart';
 part 'loading_widget.dart';
 part 'no_data_found_widget.dart';
 part 'pagination_widget.dart';
